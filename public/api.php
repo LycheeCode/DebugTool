a9549b47bd25aafdcb3696f7a5a72a745b81324c
Y!an <i@yian.me>
2018-06-11 00:19:29 +0800

    添加常用消息类型处理逻辑

--- public/api.php
+++ public/api.php
@@ -22,3 +22,110 @@ foreach ($requiredParams as $key)
         return Response::fail(40001, "缺失参数：" . $key);
     }
 }
+
+switch ($params["MsgType"])
+{
+    case 'text':
+        $msg = new Message\Text;
+        $msg->setToUserName($params["mp_username"])
+            ->setFromUserName($params["openid"])
+            ->setCreateTime(time())
+            ->setMsgId(time())
+            ->setContent($params["Msg"]["Content"]);
+        break;
+
+    case 'event':
+        if (! isset($params['Event']))
+        {
+            return Response::fail(40001, "缺少参数： Event");
+        }
+        switch ($params['Event'])
+        {
+            case 'Click':
+                $msg = new Message\Event\Click;
+                $msg->setToUserName($params["mp_username"])
+                    ->setFromUserName($params["openid"])
+                    ->setCreateTime(time())
+                    ->setMsgId(time())
+                    ->setEventKey($params['Msg']['EventKey']);
+                break;
+
+            case 'Location':
+                $msg = new Message\Event\Location;
+                $msg->setToUserName($params["mp_username"])
+                    ->setFromUserName($params["openid"])
+                    ->setCreateTime(time())
+                    ->setMsgId(time())
+                    ->setLatitude($params['Msg']['Latitude'])
+                    ->setLongitude($params['Msg']['Longitude'])
+                    ->setPrecision($params['Msg']['Precision']);
+                break;
+
+            case 'Scan':
+                $msg = new Message\Event\Scan;
+                $msg->setToUserName($params["mp_username"])
+                    ->setFromUserName($params["openid"])
+                    ->setCreateTime(time())
+                    ->setMsgId(time())
+                    ->setEventKey($params['Msg']['EventKey'])
+                    ->setTicket($params['Msg']['Ticket']);
+                break;
+
+            case 'Subscribe':
+                $msg = new Message\Event\Subscribe;
+                $msg->setToUserName($params["mp_username"])
+                    ->setFromUserName($params["openid"])
+                    ->setCreateTime(time())
+                    ->setMsgId(time());
+                if (isset($params['Msg']['EventKey']) && isset($params['Msg']['Ticket']))
+                {
+                    $msg->setEventKey($params['Msg']['EventKey'])
+                        ->setTicket($params['Msg']['Ticket']);
+                }
+                break;
+
+            case 'Unsubscribe':
+                $msg = new Message\Event\Unsubscribe;
+                $msg->setToUserName($params["mp_username"])
+                    ->setFromUserName($params["openid"])
+                    ->setCreateTime(time())
+                    ->setMsgId(time());
+                break;
+                
+            case 'View':
+                $msg = new Message\Event\View;
+                $msg->setToUserName($params["mp_username"])
+                    ->setFromUserName($params["openid"])
+                    ->setCreateTime(time())
+                    ->setMsgId(time())
+                    ->setEventKey($params['Msg']['EventKey']);
+                break;
+            
+            default:
+                return Response::fail(40002, "消息类型不正确");
+                break;
+        }
+        break;
+    
+    case 'image':
+    case 'link':
+    case 'location':
+    case 'music':
+    case 'news':
+    case 'shortvideo':
+    case 'video':
+    case 'voice':
+        // TO-DO
+        break;
+
+    default:
+        return Response::fail(40002, "消息类型不正确");
+        break;
+}
+
+$url = Sign::url($params["url"], Sign::generate($params["token"])) . "&openid=" . $params["openid"];
+
+$http = new Http;
+$result = $http->postRaw($url, $msg->toXML());
+
+$reply = Message\Auto::init($result);
